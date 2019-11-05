fb79ea950289c50d7d7d16c02728a79f0ef465fb
ddnnss <ifpass112233then>
2019-11-05 12:49:13 +0300

    lk fixes

--- order/views.py
+++ order/views.py
@@ -71,7 +71,7 @@ def pay(request):
         label= {}
         print(json.loads(req))
         ordersIds=[]
-
+        label['user'] = request.user.id
         for order in json.loads(req):
 
             encodeString = json.loads(req)[order]['id']
@@ -99,6 +99,7 @@ def pay(request):
                     return_dict['result'] = 'error'
 
                 UserLog.objects.create(user=request.user, action='Попытка : {} на сумму {}'.format(targets, totalprice))
+
                 return_dict['result'] = 'ok'
                 return_dict['totalprice'] = totalprice
                 return_dict['label'] = json.dumps(label)
@@ -126,6 +127,7 @@ def pay_complete(request):
     operation_id = req.get('operation_id')
     payment_source = ''
     order = None
+    user_id = 0
 
     if notification_type == 'p2p-incoming':
         payment_source = 'из кошелька'
@@ -133,60 +135,63 @@ def pay_complete(request):
         payment_source = 'c карты'
     if not unaccepted or codepro:
         for x in label:
-            order_id = x
-            payment_type = label[x]
+            if x == 'user':
+                user_id = int(label[x])
+            else:
+                order_id = x
+                payment_type = label[x]
 
-            try:
-                order = Order.objects.get(id=order_id)
-                print(order)
-            except:
-                print('order not found')
-                UserLog.objects.create(user=request.user,
-                                       action='Попытка оплаты не существуюшего заказа')
-                return HttpResponse(status=500)
-            if order:
-                if payment_type == '0' and not order.is_prePayed:
-                    order.is_prePayed = True
-                    order.save(force_update=True)
-                    print('order is prepay now')
+                try:
+                    order = Order.objects.get(id=order_id)
+                    print(order)
+                except:
+                    print('order not found')
+                    UserLog.objects.create(user_id=user_id,
+                                           action='Попытка оплаты не существуюшего заказа')
+                    return HttpResponse(status=500)
+                if order:
+                    if payment_type == '0' and not order.is_prePayed:
+                        order.is_prePayed = True
+                        order.save(force_update=True)
+                        print('order is prepay now')
 
-                    UserLog.objects.create(user=request.user,
-                                           action='Предоплата заказа {}'.format(order.id))
+                        UserLog.objects.create(user_id=user_id,
+                                               action='Предоплата заказа {}'.format(order.id))
 
-                    newPayment = Payment.objects.create(order=order,
-                                           user=request.user,
-                                           amount=amount,
-                                           withdraw_amount=withdraw_amount,
-                                           sender=sender,
-                                           type='Предоплата {}'.format(payment_source),
-                                           operation_id=operation_id)
-                    print(newPayment)
-                else:
-                    print('order is prepay already')
-                if payment_type == '1' and not order.is_fullPayed:
-                    order.is_fullPayed = True
-                    if order.is_prePayed:
-                        payment_type_name = 'Доплата'
+                        newPayment = Payment.objects.create(order=order,
+                                               user_id=user_id,
+                                               amount=amount,
+                                               withdraw_amount=withdraw_amount,
+                                               sender=sender,
+                                               type='Предоплата {}'.format(payment_source),
+                                               operation_id=operation_id)
+                        print(newPayment)
                     else:
-                        payment_type_name = 'Оплата'
-                    order.save(force_update=True)
-                    print('order is payed now')
-                    UserLog.objects.create(user=request.user,
-                                           action='Оплата заказа {}'.format(order.id))
-                    Payment.objects.create(order=order,
-                                           user=request.user,
-                                           amount=amount,
-                                           withdraw_amount=withdraw_amount,
-                                           sender=sender,
-                                           type='{} {}'.format(payment_type_name, payment_source),
-                                           operation_id=operation_id)
+                        print('order is prepay already')
+                    if payment_type == '1' and not order.is_fullPayed:
+                        order.is_fullPayed = True
+                        if order.is_prePayed:
+                            payment_type_name = 'Доплата'
+                        else:
+                            payment_type_name = 'Оплата'
+                        order.save(force_update=True)
+                        print('order is payed now')
+                        UserLog.objects.create(user_id=user_id,
+                                               action='Оплата заказа {}'.format(order.id))
+                        Payment.objects.create(order=order,
+                                               user_id=user_id,
+                                               amount=amount,
+                                               withdraw_amount=withdraw_amount,
+                                               sender=sender,
+                                               type='{} {}'.format(payment_type_name, payment_source),
+                                               operation_id=operation_id)
 
-                else:
-                    print('order is payed already')
+                    else:
+                        print('order is payed already')
 
         return HttpResponse(status=200)
     else:
-        UserLog.objects.create(user=request.user, action='Оплата не удалась, платеж  защищен кодом протекции или еще не проведен')
+        UserLog.objects.create(user_id=user_id, action='Оплата не удалась, платеж  защищен кодом протекции или еще не проведен')
         return HttpResponse(status=500)
 
 
